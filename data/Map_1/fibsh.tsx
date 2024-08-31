b84f8ca08b8359c335dc29309fa59228cc26d03c
Len_Senit <piotr.k.tor@gmail.com>
2024-08-31 13:19:46 +0200

    Map upgraded

---
+++ data/Map_1/fibsh.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="fibsh" tilewidth="16" tileheight="16" tilecount="10" columns="10">
+ <image source="C:/Users/piotr/Downloads/fibsh.png" width="160" height="16"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="450"/>
+   <frame tileid="1" duration="450"/>
+   <frame tileid="2" duration="450"/>
+   <frame tileid="3" duration="450"/>
+   <frame tileid="2" duration="450"/>
+   <frame tileid="1" duration="450"/>
+   <frame tileid="0" duration="450"/>
+   <frame tileid="6" duration="450"/>
+   <frame tileid="7" duration="450"/>
+   <frame tileid="8" duration="450"/>
+   <frame tileid="9" duration="450"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="450"/>
+   <frame tileid="2" duration="450"/>
+   <frame tileid="3" duration="450"/>
+   <frame tileid="4" duration="450"/>
+   <frame tileid="5" duration="450"/>
+   <frame tileid="3" duration="450"/>
+   <frame tileid="8" duration="450"/>
+   <frame tileid="9" duration="450"/>
+   <frame tileid="0" duration="450"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="0" duration="450"/>
+   <frame tileid="1" duration="450"/>
+   <frame tileid="2" duration="450"/>
+   <frame tileid="3" duration="450"/>
+   <frame tileid="4" duration="450"/>
+   <frame tileid="5" duration="450"/>
+   <frame tileid="6" duration="450"/>
+   <frame tileid="7" duration="450"/>
+   <frame tileid="8" duration="450"/>
+   <frame tileid="9" duration="450"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="4" duration="450"/>
+   <frame tileid="0" duration="450"/>
+   <frame tileid="3" duration="450"/>
+   <frame tileid="9" duration="450"/>
+   <frame tileid="2" duration="450"/>
+   <frame tileid="1" duration="450"/>
+   <frame tileid="2" duration="450"/>
+   <frame tileid="1" duration="450"/>
+   <frame tileid="6" duration="450"/>
+   <frame tileid="9" duration="450"/>
+   <frame tileid="2" duration="450"/>
+   <frame tileid="1" duration="450"/>
+   <frame tileid="0" duration="450"/>
+   <frame tileid="8" duration="450"/>
+  </animation>
+ </tile>
+</tileset>
